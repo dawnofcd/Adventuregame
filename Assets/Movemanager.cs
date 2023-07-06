49307477d62005d01c2c053e3ef49ee2acf6c016
Dawnftcd <kuhunglov3@gmail.com>
2023-07-06 16:28:54 +0700

    update jump

--- Assets/Movemanager.cs
+++ Assets/Movemanager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Tilemaps;
@@ -8,7 +8,6 @@ using UnityEngine.Timeline;
 public class Movemanager : MonoBehaviour
 {
     private float speed = 5f;
-    //float jumpforce=5f;
     Rigidbody2D rb;
     Vector2 Vector2;
     public Animator Ani;
@@ -19,25 +18,29 @@ public class Movemanager : MonoBehaviour
     public LayerMask ground;
     bool doublejump;
 
+   
+   
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //  Ani= GetComponent<Animator>();
+         Ani= GetComponent<Animator>();
     }
     private void Update()
     {
         MoveHorizal();
-        Filip();
-        Anima();
         Jumpping();
+        Ani.SetBool("jump", Input.GetKeyDown(KeyCode.Space));
+       
+      
+          
     }
 
     public virtual void MoveHorizal()
     {
         rb.velocity = new Vector2(inputManager.Instance.Move * speed, rb.velocity.y);
-        if (inputManager.Instance.Numberjump <= 2 && checkground)
-        { rb.AddForce(Vector2.up * inputManager.Instance.MoveJump, ForceMode2D.Impulse); }
-
+        Animarun();
+        Filip();
     }
     public virtual void Filip()
     {
@@ -47,17 +50,20 @@ public class Movemanager : MonoBehaviour
         { transform.localScale = new Vector3(1, 1, 1); Isfascingright = false; }
     }
 
-    void Anima()
+    void Animarun()
     {
         Ani.SetFloat("run", Mathf.Abs(inputManager.Instance.Move));
     }
-
+    
     void Jumpping()
     {
         checkground = Physics2D.OverlapCircle(Jumpaccept.position, 0.1f, ground);
         if (checkground && !Input.GetKey(KeyCode.Space))
-        {
+        {   
             doublejump = false;
+           
+            
+         
         }
         if (Input.GetKeyDown(KeyCode.Space))
         { 
@@ -65,8 +71,10 @@ public class Movemanager : MonoBehaviour
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpforce);
                 doublejump = !doublejump;
+          
             }
-        } 
+        }
+     
     } 
 }
     
